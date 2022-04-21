9d3ecaaf62bbf7a9884cfc91307f0ea3c45dde36
gwijong <drophail@daum.net>
2022-04-21 12:41:16 +0900

    20220421

--- Zombie/Assets/02.Scripts/GameManager.cs
+++ Zombie/Assets/02.Scripts/GameManager.cs
@@ -1,26 +1,57 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
-public class GameManager : MonoBehaviour
+public class GameManager : MonoBehaviour //점수와 게임오버 여부를 관리하는 게임 매니저
 {
-    public static GameManager instance;
-    public bool isGameover = false;
-    void Start()
+    public static GameManager instance  //싱글턴 접근용 프로퍼티
     {
-        if (instance == null)
+        get
         {
-            instance = this;
-        }
-        else
-        {
-            Destroy(gameObject);
+            if(m_instance == null)  //만약 싱글턴 변수에 아직 오브젝트가 할당되지 않았다면
+            {
+                m_instance = FindObjectOfType<GameManager>();  //씬에서 GameManager 오브젝트를 찾아서 할당
+            }
+            return m_instance;//싱글턴 오브젝트 반환
         }
     }
 
-    // Update is called once per frame
-    void Update()
+    private static GameManager m_instance; //싱글턴이 할당될 static 변수
+
+    private int score = 0;  //현재 게임 점수
+    public bool isGameover { get; private set;}  //게임오버 상태
+
+    private void Awake()
     {
-        
+        if(instance != this)  //씬에 싱글턴 오브젝트가 된 다른 GameManager 오브젝트가 있다면
+        {
+            Destroy(gameObject);//자신을 파괴
+        }
     }
+
+    private void Start()
+    {
+        //플레이어 캐릭터의 사망 이벤트 발생 시 게임 오버
+        FindObjectOfType<PlayerHealth>().onDeath += EndGame;  //반환값과 매개변수가 없는 델리게이트 Action에 EndGame 추가    
+    }
+
+    //점수를 추가하고 UI 갱신
+    public void AddScore(int newScore)
+    {
+        //게임오버가 아닌 상태에서만 점수 추가 가능
+        if (!isGameover)
+        {
+            //점수 추가
+            score += newScore;
+            //점수 UI 텍스트 갱신
+            UIManager.instance.UpdateScoreText(score);
+        }
+    }
+
+    //게임오버 처리
+    public void EndGame()
+    {
+        //게임오버 상태를 참으로 변경
+        isGameover = true;
+        //게임오버 UI 활성화
+        UIManager.instance.SetActiveGameoverUI(true);
+    } 
 }
--- Zombie/Assets/02.Scripts/UIManager.cs
+++ Zombie/Assets/02.Scripts/UIManager.cs
@@ -1,24 +1,64 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI; // UI 관련 코드
+using UnityEngine.SceneManagement; //씬 관리자 관련 코드
 
+/// <summary> 필요한 UI에 즉시 접근하고 변경할 수 있도록 허용하는 UI 매니저 </summary>
 public class UIManager : MonoBehaviour
 {
-    public static UIManager instance;
-
-    void Start()
+    /// <summary> 싱글턴 접근용 프로퍼티 </summary>
+    public static UIManager instance
     {
-        
+        get
+        {
+            if(m_instance == null)
+            {
+                m_instance = FindObjectOfType<UIManager>();
+            }
+            return m_instance;
+        }
     }
 
+    private static UIManager m_instance; //싱글턴이 할당될 변수
 
-    void Update()
+    /// <summary> 탄알 표시용 텍스트 </summary>
+    public Text ammoText; 
+    /// <summary> 점수 표시용 텍스트 </summary>
+    public Text scoreText;  
+    /// <summary> 적 웨이브 표시용 텍스트 </summary>
+    public Text waveText;  
+    /// <summary> 게임오버 시 활성화할 UI </summary>
+    public GameObject gameoverUI;  
+
+    /// <summary> 탄알 텍스트 갱신 </summary>
+    public void UpdateAmmoText(int magAmmo, int remainAmmo)
     {
-        
+        ammoText.text = magAmmo + "/" + remainAmmo;
     }
 
-    public void UpdateAmmoText(int magAmmo, int ammoRemain)
+    /// <summary> 점수 텍스트 갱신 </summary>
+    public void UpdateScoreText(int newScore)
     {
-
+        scoreText.text = "Score : " + newScore;
     }
+
+    /// <summary> 적 웨이브 텍스트 갱신 </summary>
+    public void UpdataWaveText(int waves, int count)
+    {
+        waveText.text = "Wave : " + waves + "\nEnemy Left : " + count; //  \n : 개행 문자
+    }
+
+    /// <summary> 게임오버 UI 활성화 </summary>
+    public void SetActiveGameoverUI(bool active)
+    {
+        gameoverUI.SetActive(active);
+    }
+
+    /// <summary> 게임 재시작 </summary>
+    public void GameRestart()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+
 }
